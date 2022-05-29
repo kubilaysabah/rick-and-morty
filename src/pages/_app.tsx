7e6587ad487bf801739c0356d49b91df66024879
Kubilay <kubilaysabah@gmail.com>
2022-05-29 09:31:34 +0300

    redux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,18 @@ import { appWithTranslation } from "next-i18next";
 // React
 import type { FC, ReactElement } from "react";
 
+// Redux
+import { Provider } from "react-redux";
+import store from "state";
+
 // Styles
 import "styles/globals.css";
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps): ReactElement<AppProps> => {
   return (
-    <Component {...pageProps} />
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
   )
 }
 
