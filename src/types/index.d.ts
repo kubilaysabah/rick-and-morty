c62483c15fe99dc51c93a1087598ae39ab3df2fc
Kubilay <kubilaysabah@gmail.com>
2022-05-29 09:31:04 +0300

    add response types

---
+++ src/types/index.d.ts
@@ -0,0 +1,48 @@
+interface Response<T> {
+    info: {
+        count: number;
+        next: string | null;
+        pages: number;
+        prev: string | null;
+    };
+    results: T[];
+}
+
+interface BaseAPI {
+    id: number;
+    name: string;
+    url: string;
+    created: string;
+}
+
+interface ICharacter extends BaseAPI {
+    status: "Alive" | "Dead" | "unknown";
+    species: string;
+    type: string;
+    gender: "Female" | "Male" | "Genderless" | "unknown";
+    origin: {
+        name: string;
+        url: string;
+    };
+    location: {
+        name: string;
+        url: string;
+    };
+    origin: {
+        name: string;
+        url: string;
+    };
+    episode: string[];
+}
+
+interface ILocation extends BaseAPI {
+    type: string;
+    dimension: string;
+    residents: string[];
+}
+
+interface IEpisode extends BaseAPI {
+    air_date: string;
+    episode: string;
+    characters: string[];
+}
